75747321e3844e4d14af64d6a242ea5a54bd5d5c
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-06-06 17:01:43 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,33 @@
 class StudentsController < ApplicationController
-  
-  def index
-    @students = Student.all
-  end
+def index
+  @students = Student.all
+end
 
-  def show
+def show
+  @student = Student.find(params[:id])
+end
+
+def new
+  @student = Student.new
+end
+
+def create
+  @student =Student.new(student_params)
+
+  if @student.save
+    redirect_to @student
+  else
+    render 'new'
+  end
+end
+
+private
+
+  def set_student
     @student = Student.find(params[:id])
   end
 
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end 
 end
